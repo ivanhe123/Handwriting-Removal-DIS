0962d3a83019f5f7941c900abd537c1ff3253786
Ivan He <108038846+ivanhe123@users.noreply.github.com>
2025-01-08 15:18:00 +0800

    Update create_masks.py

--- create_masks.py
+++ create_masks.py
@@ -2,10 +2,30 @@ import os
 import cv2
 import numpy as np
 from tqdm import tqdm
+def resize_image(image, output_path, size=1024):
+    
+    height, width = image.shape[:2]
+
+    if height > size or width > size:
+        # Calculate the scale factor
+        if height > width:
+            scale_factor = size / height
+        else:
+            scale_factor = size / width
+
+        new_dimensions = (int(width * scale_factor), int(height * scale_factor))
+        resized_image = cv2.resize(image, new_dimensions, interpolation=cv2.INTER_AREA)
+
+        return resized_image
+
+    else:
+        return image
 
 def process_image(input_path, output_path):
 
     img = cv2.imread(input_path)
+
+    img = resize_image(img)
 
     mask = cv2.inRange(img, (0, 0, 255), (0, 0, 255))
 
